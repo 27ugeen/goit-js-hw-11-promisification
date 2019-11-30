b92ca8f1ee85c7e7126e40c090b5cc8802d00568
27ugeen <27ugeen@gmail.com>
2019-11-30 12:24:42 +0200

    promises done

---
+++ task02.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+//==============================================
+// const toggleUserState = (allUsers, userName, callback) => {
+//     const updatedUsers = allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user,
+//     );
+// //   const updatedUsers = allUsers.map(user => {
+// //     if (user.name === userName) {
+// //       user.active = !user.active;
+// //       return user;
+// //     } else {
+// //       return user;
+// //     }
+// //   });
+
+//   callback(updatedUsers);
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+//====================================================
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    resolve(
+      allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      ),
+    );
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
+
+//================================================
+
+
