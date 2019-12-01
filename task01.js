92139381bcaeca5bed65d4a2d0a507f896447e4e
27ugeen <27ugeen@gmail.com>
2019-12-01 19:31:47 +0200

    fix task 01

--- task01.js
+++ task01.js
@@ -6,7 +6,11 @@
 // миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-  return Promise.resolve(ms);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
